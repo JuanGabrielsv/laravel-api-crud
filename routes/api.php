ccb331f14df64d176c37513bf1fc8664c63070fe
juangabrielsv <juangabrielsv@gmail.com>
2025-07-15 22:32:01 +0200

    Creado rutas de crud básico, comprobado en postman.

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/conciertos', function () {
+    return 'Obteniendo conciertos';
+});
+
+Route::get('/conciertos/{id}', function () {
+    return 'Obteniendo un concierto';
+});
+
+Route::post('/conciertos', function () {
+    return 'Creando concierto';
+});
+
+Route::put('/conciertos/{id}', function () {
+    return 'Editando concierto';
+});
+
+Route::delete('/conciertos/{id}', function () {
+    return 'Eliminando concierto';
+});
