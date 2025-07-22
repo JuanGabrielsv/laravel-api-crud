0fcc439ccbebcf7432f9b3debd6aa392836405a7
juangabrielsv <juangabrielsv@gmail.com>
2025-07-22 19:19:10 +0200

    Creado archivos para el modelo GeneroMusical.

---
+++ app/Http/Controllers/GeneroMusicalController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GeneroMusical;
+use App\Http\Requests\StoreGeneroMusicalRequest;
+use App\Http\Requests\UpdateGeneroMusicalRequest;
+
+class GeneroMusicalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGeneroMusicalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GeneroMusical $generoMusical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGeneroMusicalRequest $request, GeneroMusical $generoMusical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GeneroMusical $generoMusical)
+    {
+        //
+    }
+}
